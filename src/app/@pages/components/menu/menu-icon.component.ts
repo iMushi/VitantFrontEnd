bd8e30637fca2e37fe9034283b8b74bcb6767c2f
Gabriel Lopez <gabriel.ls@globant.com>
2019-04-03 12:21:05 -0600

    arreglo de links y estilo de título para mobile

--- src/app/@pages/components/menu/menu-icon.component.ts
+++ src/app/@pages/components/menu/menu-icon.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit,Input,HostBinding } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pg-menu-icon',
   templateUrl: './menu-icon.component.html',
   styleUrls: ['./menu-icon.component.scss'],
-  host:{
-    '[class]' : '_classMap'
+  host: {
+    '[class]': '_classMap'
   }
 })
 export class MenuIconComponent implements OnInit {
-  _classMap:string;
+  _classMap: string;
   @Input() IconType: string;
   @Input() IconName: string;
 
-  @Input() 
-  set ExtraClass(value:string){
-    if(value !== undefined)
-    this._classMap =this._classMap +  value;
-  }
-  
-  constructor() {
-    this._classMap = "icon-thumbnail "
+  constructor () {
+    this._classMap = 'icon-thumbnail ';
   }
 
-  ngOnInit() {
+  @Input()
+  set ExtraClass (value: string) {
+    if (value !== undefined) {
+      this._classMap = this._classMap + value;
+    }
+  }
+
+  ngOnInit () {
   }
 
 }
--- src/app/@pages/components/menu/menu.component.ts
+++ src/app/@pages/components/menu/menu.component.ts
@@ -1,14 +1,6 @@
-
-import { Component, OnInit,Input, ViewEncapsulation, HostListener } from '@angular/core';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
-import { PerfectScrollbarConfigInterface,
-  PerfectScrollbarComponent, PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
+import { AfterViewInit, Component, HostListener, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 
 @Component({
   selector: 'pg-menu-items',
@@ -16,61 +8,63 @@ import { PerfectScrollbarConfigInterface,
   styleUrls: ['./menu.component.scss'],
   animations: [
     trigger('toggleHeight', [
-        state('close', style({
-            height: '0',
-            overflow: 'hidden',
-            marginBottom:'0',
-            display:'none',
-           
-        })),
-        state('open', style({
-            display:'block',
-            height: '*',
-            marginBottom:'10px',
-            overflow: 'hidden',
-        })),
-        transition('close => open', animate('140ms ease-in')),
-        transition('open => close', animate('140ms ease-out'))
+      state('close', style({
+        height: '0',
+        overflow: 'hidden',
+        marginBottom: '0',
+        display: 'none'
+
+      })),
+      state('open', style({
+        display: 'block',
+        height: '*',
+        marginBottom: '10px',
+        overflow: 'hidden'
+      })),
+      transition('close => open', animate('140ms ease-in')),
+      transition('open => close', animate('140ms ease-out'))
     ])
   ],
-  encapsulation:ViewEncapsulation.None 
+  encapsulation: ViewEncapsulation.None
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit {
   menuItems = [];
   currentItem = null;
-  isPerfectScrollbarDisabled = false
+  isPerfectScrollbarDisabled = false;
   public config: PerfectScrollbarConfigInterface = {};
-  constructor() { }
 
-  ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-    setTimeout(()=>{
-      this.togglePerfectScrollbar();
-    })
-  }
-
-  @HostListener("window:resize", [])
-	onResize() {
-    this.togglePerfectScrollbar();
-	}
-
-  togglePerfectScrollbar(){
-    this.isPerfectScrollbarDisabled = window.innerWidth < 1025
+  constructor () {
   }
 
   @Input()
-  set Items(value) {
-    this.menuItems = value
+  set Items (value) {
+    this.menuItems = value;
   }
-  
-  toggleNavigationSub(event,item) {
-      event.preventDefault();
-      if(this.currentItem && this.currentItem != item){
-        this.currentItem["toggle"] = 'close';
-      }
-      this.currentItem = item;
-      item.toggle = (item.toggle === 'close' ? 'open' : 'close');
+
+  ngOnInit () {
+  }
+
+  ngAfterViewInit () {
+    setTimeout(() => {
+      this.togglePerfectScrollbar();
+    });
+  }
+
+  @HostListener('window:resize', [])
+  onResize () {
+    this.togglePerfectScrollbar();
+  }
+
+  togglePerfectScrollbar () {
+    this.isPerfectScrollbarDisabled = window.innerWidth < 1025;
+  }
+
+  toggleNavigationSub (event, item) {
+    event.preventDefault();
+    if (this.currentItem && this.currentItem != item) {
+      this.currentItem['toggle'] = 'close';
+    }
+    this.currentItem = item;
+    item.toggle = (item.toggle === 'close' ? 'open' : 'close');
   }
 }
