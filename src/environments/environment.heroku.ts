4bc9db6849773059b0eb7aed35ecd9185f5a6ea4
Gabriel Lopez <gabriel.ls@globant.com>
2019-05-20 20:46:26 -0500

    ambiente heroku

---
+++ src/environments/environment.heroku.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  urlServices: 'https://morning-wave-50354.herokuapp.com/api',
+  registerUser: '/register',
+  loginUser: '/login',
+  getContacts: '/getContacts',
+  registerContacts: '/registerContacts'
+};
+
+
