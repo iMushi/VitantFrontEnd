10164a3d2efaa5ac7bd2f20a03e14f1549963bb9
Gabriel Lopez <gabriel.ls@globant.com>
2019-05-26 22:10:16 -0500

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -10492,12 +10492,8 @@ module.exports = __webpack_require__.p + "Pages-icon.0fd5f549d439c281557b.woff";
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: false,
+    production: true,
     useMockUps: true,
     endpoint: 'http://localhost:8090',
     urlUserServices: '/api/user',
