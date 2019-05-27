9273c1727647cb44a5ecc2a536c961dc7d48a1dd
Gabriel Lopez <gabriel.ls@globant.com>
2019-05-26 22:07:07 -0500

    ajuste de tamaño de imagenes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  useMockUps: false,
+  useMockUps: true,
   endpoint: 'http://localhost:8090',
   urlUserServices: '/api/user',
   urlBackOfficeServices: '/api/backOffice',
   registerUser: '/register',
-  loginUser: '/loginUser.json',
+  loginUser: '/login',
   getContacts: '/getContacts',
   registerContacts: '/registerContacts',
   getRegByType: '/getRegByType'
